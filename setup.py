5f629fb27dac3e73783dcdc90a8fa69c8960c86c
Nnarol <lorand.l.juhasz@gmail.com>
2020-05-26 21:55:27 +0200

    Create a packaging mechanism based on cx-freeze
    Code fix: import matplotlib.pyplot.
    Create a basic script to be packaged (not that useful in itself).

---
+++ setup.py
@@ -0,0 +1,18 @@
+from cx_Freeze import setup, Executable
+
+# The scipy.spatial.cKDTree exclude is a workaround to a name-case conflict
+# on Windows with a related file that prevents cx_Freeze from bundling the
+# appropriate library.
+buildOptions = dict(packages = ["matplotlib", "scipy", "pandas", "numpy", "requests", "seaborn"], namespace_packages = ["mpl_toolkits"], excludes = ["scipy.spatial.cKDTree"])
+
+base = 'Console'
+
+executables = [
+    Executable('aoe2stats.py', base=base, targetName = 'aoe2stats')
+]
+
+setup(name='aoe2stats',
+      version = '1.0',
+      description = '',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
